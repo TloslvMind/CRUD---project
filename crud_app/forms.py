fe15e2fa53c7342d6e2c46510ab4c8d35db405df
Tysiachnyk <tloslvmind@gmail.com>
2024-07-29 14:56:56 +0300

    forms.widget Test

--- crud_app/forms.py
+++ crud_app/forms.py
@@ -2,5 +2,6 @@ from django import forms
 
 
 class UserForm(forms.Form):
-    name = forms.CharField(label='Имя')
-    age = forms.IntegerField(label='Ваш возраст?')
+    name = forms.CharField(max_length=100, label='Ваше имя',
+                           widget=forms.TextInput(attrs={'placeholder': 'Имя'}))
+    age = forms.IntegerField(label='Ваш возраст?', initial=18, help_text='Введите свой возраст')
